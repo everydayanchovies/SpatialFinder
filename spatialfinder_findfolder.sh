a4a05c7814b20f842b2a3743ecd694b434eb3395
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2025-10-02 13:01:50 +0200

    reorganised folder finder script

--- spatialfinder_findfolder.sh
+++ spatialfinder_findfolder.sh
@@ -1,13 +1,21 @@
 #!/bin/zsh
 
-folders=()
+searchin="Documents"
 
-mdfind -onlyin ~/Documents "kMDItemDisplayName == '$1' && (kMDItemKind == 'Folder')" | while IFS= read -r line; do
-    folders+=("$line")
-done
+foldername=$(echo "$1" | sed 's/ — Local$//')
 
-if [[ ${#folders[@]} -eq 1 ]]; then
-    echo "${folders[1]}"
+if [[ "$foldername" == "$searchin" ]]; then
+    echo $(readlink --canonicalize ~/$searchin)
 else
-    exit 1
+    folders=()
+
+    mdfind -onlyin $(readlink --canonicalize ~/$searchin) "kMDItemDisplayName == '$foldername' && (kMDItemKind == 'Folder')" | while IFS= read -r line; do
+        folders+=("$line")
+    done
+
+    if [[ ${#folders[@]} -eq 1 ]]; then
+        echo "${folders[1]}"
+    else
+        exit 1
+    fi
 fi
